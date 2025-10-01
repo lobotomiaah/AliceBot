589828c2c965249e3f9d00e8918cfe9ce7918f76
lobotomiaah <maxwelferreira@proton.me>
2025-10-01 18:22:37 -0300

    melhorado o sistema do bot e criado o comando de calculo

---
+++ commands/calculator.py
@@ -0,0 +1,45 @@
+from discord.ext import commands
+
+class Calculator(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def calc(self, ctx: commands.Context, operacao: str = None, num1: str = None, num2: str = None):
+        
+        if not operacao or not num1 or not num2:
+            await ctx.reply("Uso correto: `.calc <operacao> <num1> <num2>`\nExemplo: `.calc soma 10 5`")
+            return
+
+      
+        try:
+            num1 = float(num1.replace(",", "."))
+            num2 = float(num2.replace(",", "."))
+        except ValueError:
+            await ctx.reply("Você precisa digitar números válidos!")
+            return
+
+        resultado = None
+        operacao = operacao.lower()
+
+        
+        if operacao == "soma":
+            resultado = num1 + num2
+        elif operacao == "sub":
+            resultado = num1 - num2
+        elif operacao == "multi":
+            resultado = num1 * num2
+        elif operacao == "div":
+            if num2 != 0:
+                resultado = num1 / num2
+            else:
+                await ctx.reply("Não é possível dividir por zero!")
+                return
+        else:
+            await ctx.reply("Operação inválida! Use: soma, sub, multi, div")
+            return
+
+        await ctx.reply(f"Resultado é {resultado}")
+
+async def setup(bot):
+    await bot.add_cog(Calculator(bot))
---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import asyncio
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ O bot {bot.user} está online!")
+
+async def load_commands():
+    for filename in os.listdir("C:\\Users\\lobotomia\\Downloads\\AliceBot\\commands"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"commands.{filename[:-3]}")
+            print(f"✔ Comando {filename} carregado!")
+
+async def main():
+    async with bot:
+        await load_commands()
+        await bot.start(TOKEN)
+
+asyncio.run(main())
